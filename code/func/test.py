11aeef6eeeb99d078609c88544fce8efddc3b9d4
ly4515 <1958595931@qq.com>
2022-01-07 15:56:39 +0800

    20220107

--- code/func/test.py
+++ code/func/test.py
@@ -1,23 +1,11 @@
-import os
-from requests.api import head
-import xlsxwriter
+import csv
+import random
 
-
-path = 'D:/mine/Python/photo/'
-filenames = os.listdir(path)
-# 创建一个新的工作簿wb
-wb = xlsxwriter.Workbook('D:/mine/Python/excel/photo.xlsx')
-# 使用active获取创建工作簿wb时自动生成的sheet
-sheet = wb.add_worksheet('photo')
-url = 'jjijiji'
-n = 1
-for filename in filenames:
-    sheet.write(n, 0, filename)
-    sheet.write(n, 1, url)
-    n = n+1
-
-# 工作簿落盘
-wb.close()
-full_url = []
-full = url + list
-fu = full_url.append(full)
+with open('scores.csv', 'w', encoding='UTF-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['姓名', '语文', '数学', '英语'])
+    names = ['关羽', '张飞', '赵云', '马超', '黄忠']
+    for name in names:
+        scores = [random.randrange(50, 101) for _ in range(3)]
+        scores.insert(0, name)
+        writer.writerow(scores)
--- code/shiLI/maoPao.py
+++ code/shiLI/maoPao.py
@@ -1,4 +1,7 @@
-items = ['Python', 'Java', 'Go', 'Kotlin', 'Python']
-it = sorted(items)
-print(items)
-print(it)
+print('哈哈哈')
+print('嘻嘻嘻')
+
+print('\n\n')
+
+print('哈哈哈', end='')
+print('嘻嘻嘻')
